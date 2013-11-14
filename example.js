91cca1c155f525153c6737141e9a0dbb69cd7a6b
Kujbor <Kujbor@ya.ru>
2013-11-14 18:05:33 +0300

    Update example.js

--- example.js
+++ example.js
@@ -1,17 +1,17 @@
 /*
- * example.js - example of jQuery makeForm plugin setup
+ * Example of jQuery makeForm plugin setup
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
- (function($, _) {
+$(function() {
 
     "use strict";
 
     $.get('templates/form-controls.html', function(response) {
-        $.makeForm.setup({
+        $.form.setup({
             template: _.template(response),
-            lang: 'ru'
+            lang: fg.config.lang
         });
     });
 
-})($, _);
+})();
