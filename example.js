77b1c06fc99e1cf43045be658fc9603be0067527
Kujbor <kujbor@ya.ru>
2015-10-09 15:27:06 +0300

    Update example.js

--- example.js
+++ example.js
@@ -3,11 +3,11 @@
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
-define("example", ["jquery.form", "underscore"], function() {
+define(["jquery.form", "underscore"], function() {
 
     "use strict";
 
     $.get("template.html", function(response) {
         $.fn.form.template = _.template(response);
     });
-})();
+});
