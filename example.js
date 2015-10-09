5aa19f6a2ec77ce96b0e6964d664f5d26e203cf9
Kujbor <kujbor@ya.ru>
2015-10-09 16:03:31 +0300

    Update example.js

--- example.js
+++ example.js
@@ -3,11 +3,35 @@
  * Author Oleg Taranov aka Kujbor
  * Copyright (C) 2013: CubeComp Development
  */
-define(["jquery", "jquery.form", "underscore"], function($) {
+require(["jquery", "jquery.form", "underscore"], function($) {
 
     "use strict";
 
     $.get("template.html", function(response) {
+
         $.fn.form.template = _.template(response);
+
+        $("<form>").form({
+            first_name: {
+                title: "First name",
+                required: true,
+                type: "text"
+            },
+            last_name: {
+                title: "Last name",
+                type: "text"
+            },
+            submit: {
+                title: "Next &raquo;",
+                type: "submit"
+            }
+        }, {
+            first_name: "Oleg",
+            last_name: "Taranov"
+        }, function(data) {
+
+            $("<p class='text-success'>").text(JSON.stringify(data)).insertAfter(this);
+
+        }).appendTo("body");
     });
 });
